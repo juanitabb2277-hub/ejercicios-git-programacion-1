6d3947c6289b85d00d644cd5c3e8a97a7a5f8e0c
juanita <juanitabb2277@gmail.com>
2025-08-28 12:46:44 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        System.out.printf("git status");
+        System.out.printf("git add");
+        System.out.printf("git commit -m cambios.");
+        System.out.printf("git push origin main");
+        System.out.printf("git clone");
+        System.out.printf("git branch");
+        System.out.printf("git fetch");
+        System.out.printf("git switch -c desarollo");
+        System.out.printf("git push origin desarollo");
+
     }
 }
